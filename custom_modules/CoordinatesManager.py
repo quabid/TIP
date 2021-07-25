1dd54b7734442e8e4ef3d36a419693d876336878
quobod <quobod@gmail.com>
2021-07-25 09:00:52 -0400

    Added .ser extension to pickled file

--- custom_modules/CoordinatesManager.py
+++ custom_modules/CoordinatesManager.py
@@ -4,16 +4,16 @@ from .StatusMessenger import error, success, warning, custom
 
 
 def save_location(grid_location):
-    location_file = open('coordinates', 'wb')
+    location_file = open('coordinates.ser', 'wb')
     pickle.dump(grid_location, location_file)
     location_file.close()
 
 
 def get_location():
-    if fileExists('coordinates'):
+    if fileExists('coordinates.ser'):
         message = custom("Woo Hoo!", 190, 100, 188)
         print("\t\t{}\n\n".format(message))
-        location_file = open('coordinates', 'rb')
+        location_file = open('coordinates.ser', 'rb')
         location_coordinates = pickle.load(location_file)
         return location_coordinates
     else:
