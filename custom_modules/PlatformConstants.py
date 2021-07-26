7dda2afdcc13dc8285a88a982b62de7217887ad9
quobod <quobod@gmail.com>
2021-07-26 05:41:14 -0400

    Added platformconstants module

---
+++ custom_modules/PlatformConstants.py
@@ -0,0 +1,18 @@
+import os
+import platform
+
+# From the os module
+OS_NAME = os.name
+
+
+# From the platform module
+MACHINE_TYPE = platform.machine()
+MAC_VER = platform.mac_ver()
+NODE = platform.node()
+PLATFORM = platform.platform()
+ARCH = platform.architecture()
+PROCESSOR = platform.processor()
+RELEASE = platform.release()
+SYSTEM = platform.system()
+VERSION = platform.version()
+UNAME = platform.uname()
